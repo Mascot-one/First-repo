ea7375ffcb77b97d4fc6aba9dc1950c36f7ac746
Mascot-one <shubhankar.0727@gmail.com>
2025-04-10 01:44:22 +0530

    Mcap function added successfully

--- Mini-project.py
+++ Mini-project.py
@@ -25,35 +25,55 @@ def PE_ratio(url):
     print("Could not find P/E Ratio on the page.")
 
 
-def conditions():
+
+def Market_cap(url):
+    fakeheader = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=fakeheader)
+
+    if response.status_code != 200:
+        print("Failed to fetch data.")
+        return
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    
+    market_cap = soup.find('span', class_='number')
+    if market_cap:
+        total = market_cap.text.strip()
+        print(f"Total Market cap: {total}")
+    else:
+        print("Market cap not found on the page.")
+
+
+
+def conditions(a1):
+    
     a = 0
-    a1 = input()
-    if a1 == 'yes':
+    if a1 <= '35' :
         a = a+1
-    a2 = input("2) Is net profit increasing ? \n")
+    a2 = input("1) Is net profit increasing ? \n")
     if a2 == 'yes':
         a = a+1
-    a3 = input("3) Is the FII DII holding percentage stable or increasing ? \n")
+    a3 = input("2) Is the FII DII holding percentage stable or increasing ? \n")
     if a3 == 'yes':
         a = a+1
 
     b = 0
-    b1 = input("4) Is the cash flow positive ? \n")
+    b1 = input("3) Is the cash flow positive ? \n")
     if b1 == 'yes':
         b = b+1
-    b2 = input("5) Check Debt to Equity ratio less than 2 \n")
+    b2 = input("4) Check Debt to Equity ratio less than 2 \n")
     if b2 == 'yes':
         b = b+1
-    b3 = input("6) Check the intrinsic value nearby it's stock price  \n")
+    b3 = input("5) Check the intrinsic value nearby it's stock price  \n")
     if b3 == 'yes':
         b = b+1
 
     c = 0
-    c1 = input("7) Sector wise and marketcap wise, is the stock standing in top 3 ? \n")
+    c1 = input("6) Sector wise and marketcap wise, is the stock standing in top 3 ? \n")
     if c1 == 'yes':
         c = c+1
-    c2 = input("8) Read pros and cons if pros maximun then type 'yes' else 'no' \n")
-
+    c2 = input("7) Read pros and cons if pros maximun then type 'yes' else 'no' \n")
     if c2 == 'yes':
         c = c+1
    
@@ -70,18 +90,18 @@ if (read == 'start'):
     n = int(input("How many stocks u want in your portfolio\n"))
     url = input("Paste Screener.in stock URL: ").strip()
     if "https://www.screener.in/company/" in url:
+        Market_cap(url)
         a1 = PE_ratio(url)
         print("P/E Ratio : ",a1)
         i = 1
         while i <= n :
                 print("=== STOCK NO.",i,"===")
                 print("Type answers only in 'yes' or 'no'")
-                conditions()
+                conditions(a1)
                 i += 1
        
     else :
         print("Invalid Screener URL. please paste it correct")
-
-  
+ 
 else :
     print("Read again")
